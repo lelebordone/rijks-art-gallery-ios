dcdcea64862b31854112afc4f74a026aed50baf4
Emanuele Bordone <e.bordone1997@gmail.com>
2022-02-22 10:38:27 +0100

    Create base storyboard-less Xcode project

---
+++ RijksArtGallery/RijksArtGallery/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  RijksArtGallery
+//
+//  Created by Emanuele Bordone on 22/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
