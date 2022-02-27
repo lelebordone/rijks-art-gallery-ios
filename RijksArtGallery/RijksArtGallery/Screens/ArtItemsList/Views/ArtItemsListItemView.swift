e1f3e62b8d0a361159bab61485f5cd9e90db7604
Emanuele Bordone <e.bordone1997@gmail.com>
2022-02-27 13:33:32 +0100

    Add art items list collection view items subtitle UI component

--- RijksArtGallery/RijksArtGallery/Screens/ArtItemsList/Views/ArtItemsListItemView.swift
+++ RijksArtGallery/RijksArtGallery/Screens/ArtItemsList/Views/ArtItemsListItemView.swift
@@ -4,12 +4,32 @@ class ArtItemsListItemView: UIView {
     // MARK: - Properties
     private let containerStackView: UIStackView = {
         let stackView = UIStackView()
+        stackView.axis = .vertical
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
     
+    private let headerStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.spacing = 8
+        stackView.distribution = .equalSpacing
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.layoutMargins = .init(top: 16, left: 16, bottom: 0, right: 16)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        return stackView
+    }()
+    
     private let titleLabel: UILabel = {
-        let label = RijksTitleLabel()
+        let label = RijksTitleLabel(fontSize: 17)
+        return label
+    }()
+    
+    private let subtitleLable: UILabel = {
+        let label = RijksDescriptionLabel(fontSize: 13)
         return label
     }()
     
@@ -28,9 +48,14 @@ class ArtItemsListItemView: UIView {
     private func setupUI() {
         translatesAutoresizingMaskIntoConstraints = false
         
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.systemGray2.withAlphaComponent(0.2).cgColor
+        layer.cornerRadius = 2
+        
         addContentView(containerStackView)
         
-        containerStackView.addArrangedSubview(titleLabel)
+        [headerStackView, UIView()].forEach { containerStackView.addArrangedSubview($0) }
+        [titleLabel, subtitleLable].forEach { headerStackView.addArrangedSubview($0) }
     }
 }
 
@@ -38,5 +63,6 @@ class ArtItemsListItemView: UIView {
 extension ArtItemsListItemView {
     func configure(with model: ArtItemCompact) {
         titleLabel.text = model.title
+        subtitleLable.text = model.longTitle
     }
 }
