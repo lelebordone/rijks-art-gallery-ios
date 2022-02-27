20a39da4bd238bda09c7863cbfa60fbee6dc2668
Emanuele Bordone <e.bordone1997@gmail.com>
2022-02-27 16:10:00 +0100

    Add collection view delegate adherence and move delegate/data source code into separate file

---
+++ RijksArtGallery/RijksArtGallery/Screens/ArtItemsList/Extensions/ArtItemsListViewController+.swift
@@ -0,0 +1,58 @@
+import UIKit
+
+// MARK: - Collection View Data Source
+extension ArtItemsListViewController: UICollectionViewDataSource {
+    // MARK: Number of sections and items
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        viewModel.sectionedDataSource.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        guard section < viewModel.sectionedDataSource.count else { return 0 }
+        
+        return viewModel.sectionedDataSource[section].items.count
+    }
+    
+    // MARK: Header
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            guard
+                let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader,
+                                                                             withReuseIdentifier: ArtItemsListHeaderView.identifier,
+                                                                             for: indexPath) as? ArtItemsListHeaderView
+            else { return UICollectionReusableView() }
+            
+            let headerTitle = viewModel.sectionedDataSource[indexPath.section].title
+            header.configure(with: headerTitle)
+            
+            return header
+        default:
+            return UICollectionReusableView()
+        }
+    }
+
+    // MARK: Cell
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let identifier = ArtItemsListCollectionViewCell.identifier
+        guard
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier,
+                                                          for: indexPath) as? ArtItemsListCollectionViewCell
+        else {
+            return UICollectionViewCell()
+        }
+        
+        let artItem = viewModel.sectionedDataSource[indexPath.section].items[indexPath.item]
+        cell.configure(with: artItem)
+        
+        return cell
+    }
+}
+
+// MARK: - Collection View Delegate
+extension ArtItemsListViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let selectedItem = viewModel.sectionedDataSource[indexPath.section].items[indexPath.item]
+        print(selectedItem)
+    }
+}
